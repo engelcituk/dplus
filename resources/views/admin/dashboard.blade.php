959ee3ad53c9c4e0509f076c1d1798eed2c58848
cituk <programaciondyn3.mx@sandos.com>
2020-04-21 02:03:41 -0500

    pagina login

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,4 +2,4 @@
 
 @section('content')
     <h1>Dashboard</h1>
-@stop
+@endsection
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,101 @@
-@extends('layouts.app')
+<!DOCTYPE html>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>
+            Login - - SmartAdmin v4.0.1
+        </title>
+        <meta name="description" content="Login">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
+        <!-- Call App Mode on ios devices -->
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <!-- Remove Tap Highlight on Windows Phone IE -->
+        <meta name="msapplication-tap-highlight" content="no">
+        <!-- base css -->
+        
+        <link rel="stylesheet" media="screen, print"  href="{{ asset('smartadmin/css/vendors.bundle.css') }}">
+        <link rel="stylesheet" media="screen, print" href="{{ asset('smartadmin/css/app.bundle.css') }}">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+        <link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+        <!-- Optional: page related CSS-->
+        <link rel="stylesheet" media="screen, print" href="{{ asset('smartadmin/css/page-login.css') }}">
+    </head>
+    <body>
+        <div class="blankpage-form-field">
+            <div class="page-logo m-0 w-100 align-items-center justify-content-center rounded border-bottom-left-radius-0 border-bottom-right-radius-0 px-4">
+                <a href="javascript:void(0)" class="page-logo-link press-scale-down d-flex align-items-center">
+                    <img src="smartadmin/img/logo.png" alt="SmartAdmin WebApp" aria-roledescription="logo">
+                    <span class="page-logo-text mr-1">SmartAdmin WebApp</span>
+                    <i class="fal fa-angle-down d-inline-block ml-1 fs-lg color-primary-300"></i>
+                </a>
+            </div>
+            <div class="card p-4 border-top-left-radius-0 border-top-right-radius-0">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="form-group">
+                        <label class="form-label" for="username">Username</label>
+                        
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                        <span class="help-block">
+                            Your unique username to app
+                        </span>
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label" for="password">Password</label>
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
+                        <span class="help-block">
+                            Your password
+                        </span>
+                    </div>
+                    <div class="form-group text-left">
+                        <div class="custom-control custom-checkbox">
+                           
+                            <input type="checkbox" class="custom-control-input" name="remember" id="rememberme" {{ old('remember') ? 'checked' : '' }}>
+                            <label class="custom-control-label" for="rememberme"> Remember me for the next 30 days</label>
                         </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
+                    <button type="submit" class="btn btn-primary float-right">
+                        {{ __('Login') }}
+                    </button>
+                    
+                </form>
+            </div>
+            <div class="blankpage-footer text-center">
+                <a href="#"><strong>Recover Password</strong></a> | <a href="#"><strong>Register Account</strong></a>
+            </div>
+        </div>
+        <div class="login-footer p-2">
+            <div class="row">
+                <div class="col col-sm-12 text-center">
+                    <i><strong>System Message:</strong> You were logged out from 198.164.246.1 on Saturday, March, 2017 at 10.56AM</i>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+        <video poster="{{ asset('smartadmin/img/backgrounds/clouds.png')}}" id="bgvid" playsinline autoplay muted loop>
+            <source src="{{ asset('smartadmin/media/video/cc.webm')}}" type="video/webm">
+            <source src="{{ asset('smartadmin/media/video/cc.mp4')}}" type="video/mp4">
+        </video>
+       
+        
+        <script src="{{ asset('smartadmin/js/vendors.bundle.js') }}"></script>
+        <script src="{{ asset('smartadmin/js/app.bundle.js') }}" ></script>
+        <!-- Page related scripts -->
+    </body>
+</html>
