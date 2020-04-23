c57fba03b9dc3aac5b9df5a0937b240717d38b7a
cituk <programaciondyn3.mx@sandos.com>
2020-04-22 19:55:39 -0500

    grupo de rutas, pagescontroller, admincontroller

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
+Route::get('/', 'PagesController@home')->name('inicio');
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('admin', function(){
-    return view('admin.dashboard');
-})->middleware('auth');
+
+Route::group([
+    'prefix'=>'admin', //prefijo para no poner admin/posts
+    'namespace'=>'admin',//namespace para no poner admin\PostsController@index
+    'middleware'=>'auth'],//midleware para controlar el acceso
+function(){
+    Route::get('/', 'AdminController@index')->name('dashboard'); 
+
+});
