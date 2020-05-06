c40f09a122fb96066f696328f4ada131a9eaa26e
cituk <programaciondyn3.mx@sandos.com>
2020-05-06 00:14:03 -0500

    se modifica los seeder de users para asociarlos con un rol

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\User;
+use Spatie\Permission\Models\Role;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,18 +13,29 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        Role::truncate();
+        User::truncate();
+
+        $adminRole = Role::create(['name'=>'Admin']);
+        $sellerRole = Role::create(['name'=>'Seller ']);
+
         
-        $user = new User;
-        $user->name="Cituk Caamal 1";
-        $user->email='citukcaamal@gmail.com';
-        $user->password=bcrypt('wKyh5vfL3SM5eTn');
-        $user->save();
-       
-        $user = new User;
-        $user->name="Cituk Caamal 2";
-        $user->email='citukcaamal1@gmail.com';
-        $user->password=bcrypt('tXy3eEp9z4TbqnG');
-        $user->save();
+        $admin = new User;
+        $admin->name="Cituk Caamal 1";
+        $admin->email='citukcaamal@gmail.com';
+        $admin->password=bcrypt('wKyh5vfL3SM5eTn');
+        $admin->save();
+
+        $admin->assignRole($adminRole);// le asignamos el rol de admin
+
+        $seller = new User;
+        $seller->name="Cituk Caamal 2";
+        $seller->email='citukcaamal1@gmail.com';
+        $seller->password=bcrypt('tXy3eEp9z4TbqnG');
+        $seller->save();
+
+        $seller->assignRole($sellerRole);// le asignamos el rol de vendedor 
+
 
         $user = new User;
         $user->name="Cituk Caamal 3";
